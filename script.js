cbafbf7c2754a769f10a5e0c5916058381ca4997
Shakir Ansari <linkg518@gmail.com>
2023-09-28 06:30:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let container = document.querySelector('.cont');
+var btn = document.querySelector(".btn");
+var scrolled = false;
+
+btn.addEventListener('click', function () {
+    if (!scrolled) {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+        setTimeout(function(){
+        btn.innerHTML = '<iconify-icon icon="pajamas:scroll-up"></iconify-icon>';
+        } ,850)
+
+    } else {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+        setTimeout(function(){
+                btn.innerHTML = '<iconify-icon icon="pajamas:scroll-down"></iconify-icon>';
+        },850)
+
+    }
+    scrolled = !scrolled;
+});
+for (var i = 2; i <= 25; i++) {
+    var newImg = document.createElement('img');
+    newImg.src = 'img/img' + i + '.jpg';
+    container.appendChild(newImg);
+}
